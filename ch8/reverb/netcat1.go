646cf15ed5f3ebc017a076e8fd90adc9cd764544
tcm <chaoming2015xaut@163.com>
2019-10-18 11:37:25 +0800

    reverb

---
+++ ch8/reverb/netcat1.go
@@ -0,0 +1,27 @@
+//回声程序的client
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main(){
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer conn.Close()
+	go mustCopy1(os.Stdout, conn)
+	mustCopy1(conn,os.Stdin)
+}
+
+func mustCopy1(dst io.Writer, src io.Reader) {
+	if _,err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
+}
+
---
+++ ch8/reverb/reverb1.go
@@ -0,0 +1,47 @@
+//并发的回声程序
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"time"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	for {
+		conn ,err := listener.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+
+		go handleConn1(conn)
+	}
+}
+
+
+func handleConn1(c net.Conn) {
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		go echo(c, input.Text(), 1*time.Second)
+	}
+	c.Close()
+}
+
+func echo(c net.Conn, shout string, delay time.Duration){
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t",shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
